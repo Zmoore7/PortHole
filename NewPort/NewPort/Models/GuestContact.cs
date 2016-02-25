e7f0d78f58b00967cbdf648f3a78b53bffeeb632
Zach <moore.prestige.worldwide@gmail.com>
2016-02-25 13:56:10 -0600

    almost back to normal

---
+++ NewPort/NewPort/Models/GuestContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NewPort.Models
+{
+    public class GuestContact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email_Id { get; set; }
+    }
+
+    public class GuestDBContext : DbContext
+    {
+        public DbSet<GuestContact> Guests { get; set; }
+    }
+}
